d86e4a05717f626e5870a84d43c2194cd3d18c95
Toufiqur Chowdhury <htr.letun@gmail.com>
2020-08-29 01:35:11 +0200

    DataStorage: removed oo7 Bond, implemented RxJS and added comments

--- DataStorage.js
+++ DataStorage.js
@@ -1,6 +1,5 @@
 import { isDefined, isStr, mapSearch, isMap, isValidNumber, mapSort, isArr } from './utils'
-import { Bond } from 'oo7'
-import uuid from 'uuid'
+import { BehaviorSubject, Subject } from 'rxjs'
 
 let storage, isNode
 try {
@@ -8,7 +7,7 @@ try {
     storage = localStorage
     isNode = false
 } catch (e) {
-    // for node server
+    // for NodeJS server
     const nls = require('node-localstorage')
     const STORAGE_PATH = process.env.STORAGE_PATH || './server/data'
     isNode = true
@@ -16,6 +15,14 @@ try {
     storage = new nls.LocalStorage(STORAGE_PATH, 500 * 1024 * 1024)
 }
 
+/**
+ * @name    read
+ * @summary read from storage (JSON file if NodeJS, otherwise, browser LocalStorage)
+ *
+ * @param   {String} key file name (NodeJS) or property key (LocalStorage)
+ * 
+ * @returns {Map}        retieved data
+ */
 const read = key => {
     try {
         const data = JSON.parse(storage.getItem(key) || '[]')
@@ -24,6 +31,13 @@ const read = key => {
         console.log(`Invalid JSON on ${isNode && key ? 'file' : 'localStorage key'}: ${key}`)
     }
 }
+/**
+ * @name    write
+ * @summary write to storage (JSON file if NodeJS, otherwise, browser LocalStorage)
+ * 
+ * @param   {String}  key file name (NodeJS) or property key (LocalStorage)
+ * @param   {*}       value will be converted to JSON string
+ */
 const write = (key, value) => {
     // invalid key: ignore request
     if (!isStr(key)) return false
@@ -32,24 +46,40 @@ const write = (key, value) => {
 }
 
 export default class DataStorage {
+    /**
+     * @name DataStorage
+     * @summary a wrapper to read/write to LocalStorage (browser) or JSON file (NodeJS) with added features.
+     * @description Notes:
+     *  - this is a key-value storage that mimics the structure of `Map` and add extra functionalities like search.
+     *  - `name` is not supplied: `disableCache` will always be assumed true
+     *  - `disableCache = true`: reads once from storage and and only write when necessary.
+     *  - `disableCache = false`: data is never preserved in-memory and every read/write 
+     *      operation will be directly from/to the appropriate storage
+     *
+     * @param {String}  name filename (NodeJS) or property name (browser LocalStorage).
+     * @param {Boolean} disableCache (optional) Whether to keep data in-memory. Default: false
+     */
     constructor(name, disableCache = false) {
-        this.bond = new Bond().defaultTo(uuid.v1())
         this.name = name
-        // whether to disable data cache
-        this.disableCache = disableCache
-        this.Type = Map
-        this.data = new this.Type()
-        if (name && !disableCache) {
-            this.data = read(this.name)
+        this.disableCache = name && disableCache
+        if (this.disableCache) {
+            // only updates whenever this.set() or this.setAll() is invoked (directly or indirectly)
+            this.rxData = new Subject()
+            this.size = 0
+            return
         }
-        this.size = this.data.size
+        this.rxData = new BehaviorSubject(read(this.name))
+        this.size = this.rxData.value.size
     }
 
-    _updateBond() {
-        setTimeout(() => this.bond.changed(uuid.v1()))
-    }
-
-    // delete one or more items by key
+    /**
+     * @name    delete
+     * @summary delete one or more items by their respective keys
+     * 
+     * @param   {Array|String} keys one or more keys
+     * 
+     * @returns {DataStorage}  reference to the DataStorage instance
+     */
     delete(keys = []) {
         const data = this.getAll()
         keys = isArr(keys) ? keys : [keys]
@@ -57,32 +87,52 @@ export default class DataStorage {
         if (!keys.length) return this
         keys.forEach(key => data.delete(key))
 
-        if (!this.disableCache || !this.name) {
-            this.data = data
+        if (!this.disableCache) {
+            this.rxData.next(data)
         }
         this.name && write(this.name, data)
         this.size = data.size
-        this._updateBond()
         return this
     }
 
-    // returns first item matching criteria
+    /**
+     * @name    find
+     * @summary find the first item matching criteria. Uniqueness is not guaranteed.
+     *
+     * @param   {Object}  keyValues  Object with property names and the the value to match
+     * @param   {Boolean} matchExact (optional) fulltext or partial search. Default: false
+     * @param   {Boolean} matchAll   (optional) AND/OR operation for keys in @keyValues. Default: false
+     * @param   {Boolean} ignoreCase (optional) case-sensitivity of the search. Default: false
+     */
     find(keyValues, matchExact, matchAll, ignoreCase) {
         const result = this.search(keyValues, matchExact, matchAll, ignoreCase, 1)
         return result.size === 0 ? null : Array.from(result)[0][1]
     }
 
+    /**
+     * @name    get
+     * @summary get item by key
+     * 
+     * @param   {String} key 
+     * 
+     * @returns {*} value stored for the supplied @key
+     */
     get(key) {
         return this.getAll().get(key)
     }
 
-    getAll() {
-        if (!this.name || !this.disableCache) return this.data
+    /**
+     * @name    forceRead
+     * @summary force read from storage
+     * 
+     * @param   {Boolean} forceRead 
+     * 
+     * @returns {Map} data
+     */
+    getAll(forceRead = false) {
+        if (!forceRead && !this.disableCache) return this.rxData.value
         const data = read(this.name)
         this.size = data.size
-        if (!this.disableCache) {
-            this.data = data
-        }
         return data
     }
 
@@ -99,29 +149,52 @@ export default class DataStorage {
         return this.toArray().map(callback)
     }
 
-    search(keyValues, matchExact, matchAll, ignoreCase, limit = 0) {
+    /**
+     * @name    search
+     * @summary partial or fulltext search on storage data
+     * 
+     * @param   {Object}  keyValues  Object with property names and the the value to match
+     * @param   {Boolean} matchExact (optional) fulltext or partial search. Default: false
+     * @param   {Boolean} matchAll   (optional) AND/OR operation for keys in @keyValues. Default: false
+     * @param   {Boolean} ignoreCase (optional) case-sensitivity of the search. Default: false.
+     * @param   {Number}  limit      (optional) limits number of results. Default: 0 (no limit)
+     * 
+     * @returns {Map}     result
+     */
+    search(keyValues, matchExact = false, matchAll = false, ignoreCase = false, limit = 0) {
         const result = mapSearch(this.getAll(), keyValues, matchExact, matchAll, ignoreCase)
         const doLimit = isValidNumber(limit) && limit > 0 && result.size > limit
         return !doLimit ? result : new Map(Array.from(result).slice(0, limit))
     }
 
+    /**
+     * @name    set
+     * @summary save/update an item
+     * 
+     * @param   {String|Number|Boolean} key 
+     * @param   {*} value 
+     * 
+     * @returns {DataStorage} reference to the DataStorage instance
+     */
     set(key, value) {
         if (!isDefined(key)) return this
         const data = this.getAll()
         data.set(key, value)
         this.name && write(this.name, data)
         this.size = data.size
-        this._updateBond()
+        this.rxData.next(data)
         return this
     }
 
-    // save many items at once
-    //
-    // Params:
-    // @data        Map: list of items
-    // @override    boolean: whether to override or merge with existing data
-    //
-    // returns      DataStorage
+    /**
+     * @name    setAll
+     * @summary set multiple items at one go
+     * 
+     * @param   {Map}     data     list of items
+     * @param   {Boolean} override whether to override or merge with existing data
+     * 
+     * @returns {DataStorage}      reference to the DataStorage instance
+     */
     setAll(data, override = true) {
         if (!isMap(data)) return this
         if (!override) {
@@ -131,23 +204,22 @@ export default class DataStorage {
                 .forEach(([key, value]) => existing.set(key, value))
             data = existing // merged value
         }
-        if (!this.disableCache || !this.name) {
-            this.data = data
-        }
+
+        this.rxData.next(data)
         this.name && write(this.name, data)
-        this._updateBond()
         return this
     }
 
-    // sort data by key or simply reverse the entire list
-    //
-    // Params:
-    // @reverse     boolean: default = false
-    // @key         string: (optional) sort by specific key
-    // @save        boolean: (optional) whether to save sorted data to storage. default = false
-    // @data        Map: (optional) if supplied and save is truthy, WILL OVERRIDE all existing data
-    //
-    // Returns Map
+    /**
+     * @name    sort
+     * @summary sort data by key or simply reverse the entire list. Optionally, save sorted data to storage.
+     * 
+     * @param   {Boolean} reverse whether to reverse reverse sort. Deafult: false
+     * @param   {String}  key     (optional) sort by specific key. If `!key && !!reverse`, reverse the entire list.
+     * @param   {Boolean} save    (optional) whether to save sorted data to storage. Default: false
+     * 
+     * @retuns  {Map}
+     */
     sort(key, reverse = false, save = false) {
         let data = this.getAll()
         if (!key && !reverse) return data // nothing to do
@@ -158,10 +230,25 @@ export default class DataStorage {
         return data
     }
 
+    /**
+     * @name    toArray
+     * @summary convert list of items (Map) to 2D Array
+     * 
+     * @returns {Array}
+     */
     toArray() {
         return Array.from(this.getAll())
     }
 
+    /**
+     * @name    toString
+     * @summary converts list of items (Map) to JSON string of 2D Array 
+     * 
+     * @param   {Function}  replacer (optional) for use with `JSON.stringify`. Default: null
+     * @param   {Number}    spacing  (optional) for use with `JSON.stringify`. Default: 0
+     * 
+     * @returns {String}    JSON string
+     */
     toString(replacer = null, spacing = 0) {
         return JSON.stringify(this.toArray(), replacer, spacing)
     }
