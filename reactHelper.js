04e000760d6550102ecfa0f19d220125d6afd79b
Toufiqur Chowdhury <htr.letun@gmail.com>
2023-01-08 23:36:30 +0300

    CRUD

--- reactHelper.js
+++ reactHelper.js
@@ -308,14 +308,17 @@ export const usePromise = (promise, resultModifier, errorModifier) => {
  * 
  * @returns {Object} { message, result, unsubscribe }
  */
-export const useQueryBlockchain = (connection, func, args = [], multi, resultModifier, subscribe = true, print) => {
-    const [data, setData] = useState({
-        message: {
-            content: 'Loading...',
-            icon: true,
-            status: 'loading',
-        }
-    })
+export const useQueryBlockchain = (
+    connection,
+    func,
+    args = [],
+    multi,
+    resultModifier,
+    subscribe = true,
+    loadingText = 'Loading ...',
+    print,
+) => {
+    const [data, setData] = useState({})
 
     useEffect(() => {
         let mounted = true
@@ -366,9 +369,18 @@ export const useQueryBlockchain = (connection, func, args = [], multi, resultMod
         } else {
             // args[args.indexOf(callback)] = handleResult
         }
-        func && PromisE(connection)
-            .then(handleConnection)
-            .catch(handleError)
+        if (func) {
+            setData({
+                message: {
+                    content: loadingText,
+                    icon: true,
+                    status: 'loading',
+                }
+            })
+            PromisE(connection)
+                .then(handleConnection)
+                .catch(handleError)
+        }
 
         return () => {
             mounted = false
