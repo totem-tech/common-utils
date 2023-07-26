a9027ce605fa3c1cb044fa08494d4336065096ca
Toufiqur Chowdhury <htr.letun@gmail.com>
2023-07-26 13:28:03 +0300

    Validator: accept array for `regex` validation

--- validator.js
+++ validator.js
@@ -18,7 +18,7 @@ import {
     isValidURL,
 } from './utils'
 
-export let messages = {
+export const messages = {
     accept: 'value not acceptable',
     array: 'valid array required',
     boolean: 'boolean value required',
@@ -77,11 +77,13 @@ const errorConcat = (message, ...suffix) => {
  * @name    setMessages
  * @summary Overrides default error messages with custom/translated error messages
  * 
- * @param   {Object} msgObj Object with custom/translated messages. Must contain the correct keys.
+ * @param   {Object} messagesOverrides Object with custom/translated messages. Must contain the correct keys.
  */
-export const setMessages = msgObj => {
-    if (!isObj(msgObj)) return
-    messages = { ...messages, ...msgObj }
+export const setMessages = (messagesOverrides = {}) => {
+    isObj(messagesOverrides) && Object
+        .keys(messagesOverrides)
+        .forEach(key => messages[key] = messagesOverrides[key])
+    return messages
 }
 
 // if msg is falsy, returns true
@@ -118,7 +120,7 @@ export const validate = (value, config, customMessages = {}) => {
         .forEach(key => errorMsgs[key] = errorMsgs[key] || true)
     try {
         let err
-        const {
+        let {
             accept,
             decimals,
             config: childConf,
@@ -274,6 +276,8 @@ export const validate = (value, config, customMessages = {}) => {
             if (!valid) return _msgOrTrue(errorMsgs.reject)
         }
 
+        // if regex is an Array, assume it as arguments to instantiate `RegExp` object
+        if (isArr(regex)) regex = new RegExp(...regex)
         // validate regex expression
         if (regex instanceof RegExp && !regex.test(value)) return _msgOrTrue(errorMsgs.regex)
 
@@ -332,7 +336,7 @@ export const validateObj = (obj = {}, config = {}, failFast = true, includeLabel
         const keys = Object.keys(config)
         let errors = {}
 
-        for (let i = 0; i < keys.length; i++) {
+        for (let i = 0;i < keys.length;i++) {
             const key = keys[i]
             const value = obj[key]
             const keyConf = config[key]
